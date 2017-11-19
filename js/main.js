43d52fc5cbc8b29e79e3b71dbbdeadc7dd350d13
Alexsandro Pohren <alexsandroantonio65@outlook.com>
2017-11-19 11:44:37 -0200

    Refatorando o blog com css e javascript, finalizandro Parcial II - WEB

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    recover();
+});
+
+function addDatos() {
+    localStorage[$('#clave').val()] = $('#valor').val();
+
+    $('#data').empty();
+
+    recover();
+
+    $("#clave").val('');
+    $("#valor").val('');
+
+};
+function cleanDatos() {
+    localStorage.clear();
+    $('#data').empty();
+}
+
+function recover() {
+    for (let clave in localStorage) {
+        let dato_commit = localStorage.getItem(clave);
+        $('#data').append("<div class ='form-group lista'><h3>" + clave + "</h3><p>" + dato_commit + "</p></div>");
+    }
+
+
+
+}
